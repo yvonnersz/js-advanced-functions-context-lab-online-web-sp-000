cd25caa0945a7e5c858f88f0d7ce82ab1ade8549
Yvonne M Pham <yvonnersz@gmail.com>
2020-08-11 06:03:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -96,5 +96,5 @@ function wagesEarnedOnDate(datestamp) {
 }
 
 function findEmployeeByFirstName(arrayEmployeeRecords, firstName) {
-
+  return arrayEmployeeRecords.find(hash => hash.firstName === firstName)
 }
