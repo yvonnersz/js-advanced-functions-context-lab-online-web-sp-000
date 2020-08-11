ee4667635dd5a2aba3afc0c5c0ddbecd9414dfba
Yvonne M Pham <yvonnersz@gmail.com>
2020-08-11 06:25:31 +0000

    Done.

--- index.js
+++ index.js
@@ -20,3 +20,85 @@ let allWagesFor = function () {
 
     return payable
 }
+
+function createEmployeeRecord(employeeArray) {
+  let employeeHash = {}
+
+  employeeHash.firstName = employeeArray[0]
+  employeeHash.familyName = employeeArray[1]
+  employeeHash.title = employeeArray[2]
+  employeeHash.payPerHour = employeeArray[3]
+  employeeHash.timeInEvents = []
+  employeeHash.timeOutEvents = []
+
+  return employeeHash
+}
+
+function createEmployeeRecords(employeeArrays) {
+  return employeeArrays.map(employeeArray => createEmployeeRecord(employeeArray))
+}
+
+function createTimeInEvent(datestamp) {
+  // 2014-02-28 1400
+
+  let dateHash = {}
+
+  dateHash.type = "TimeIn"
+  dateHash.hour = parseInt(datestamp.split(" ")[1])
+  dateHash.date = datestamp.split(" ")[0]
+
+  this.timeInEvents.push(dateHash)
+  return this
+}
+
+function createTimeOutEvent(datestamp) {
+  // 2014-02-28 1400
+
+  let dateHash = {}
+
+  dateHash.type = "TimeOut"
+  dateHash.hour = parseInt(datestamp.split(" ")[1])
+  dateHash.date = datestamp.split(" ")[0]
+
+  this.timeOutEvents.push(dateHash)
+  return this
+}
+
+function hoursWorkedOnDate(datestamp) {
+  // { firstName: 'Julius',
+  // familyName: 'Caesar',
+  // title: 'General',
+  // payPerHour: 1000,
+  // timeInEvents: [ { type: 'TimeIn', hour: 900, date: '44-03-15' } ],
+  // timeOutEvents: [ { type: 'TimeOut', hour: 1100, date: '44-03-15' } ] }
+
+  // 44-03-15
+
+  let timeIn = this.timeInEvents.find(hash => hash.date === datestamp).hour
+  let timeOut = this.timeOutEvents.find(hash => hash.date === datestamp).hour
+  let hoursWorked = (timeOut-timeIn)/100
+
+  return hoursWorked
+}
+
+function wagesEarnedOnDate(datestamp) {
+  // { firstName: 'Julius',
+  //   familyName: 'Caesar',
+  //   title: 'General',
+  //   payPerHour: 27,
+  //   timeInEvents: [ { type: 'TimeIn', hour: 900, date: '44-03-15' } ],
+  //   timeOutEvents: [ { type: 'TimeOut', hour: 1100, date: '44-03-15' } ] }
+
+  // 44-03-15
+
+  let wagesEarned = hoursWorkedOnDate.call(this, datestamp) * this.payPerHour
+  return wagesEarned
+}
+
+const calculatePayroll = function(arrayEmployeeRecords) {
+  return arrayEmployeeRecords.reduce((total, employee) => total + allWagesFor.call(employee), 0)
+}
+
+function findEmployeeByFirstName(arrayEmployeeRecords, firstName) {
+  return arrayEmployeeRecords.find(hash => hash.firstName === firstName)
+}
